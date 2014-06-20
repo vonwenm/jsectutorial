0a109342789803cd2f248375557316af8810129c
Marcelle von Wendland <mvw@hivves.com>
2014-06-20 12:47:37 +0100

    start11

---
+++ DSED/dsed02.java
@@ -0,0 +1,23 @@
+import java.security.Key;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SealedObject;
+
+public class dsed02 {
+  public static void main(String[] args) throws Exception {
+    String creditCard = "1234567890";
+
+    KeyGenerator keyGenerator = KeyGenerator.getInstance("DESede");
+
+    Key key = keyGenerator.generateKey();
+    Cipher cipher = Cipher.getInstance("DESede");
+    cipher.init(Cipher.ENCRYPT_MODE, key);
+
+    SealedObject so = new SealedObject(creditCard, cipher);
+
+    String unencryptedCreditCard = (String) so.getObject(key);
+
+    System.out.println(unencryptedCreditCard);
+  }
+}
