6f65049521fcfc5881504b1c163d92635fb2a21f
Marcelle von Wendland <mvw@hivves.com>
2014-06-20 12:23:19 +0100

    start08

--- CYST/cyst01.java
+++ CYST/cyst01.java
@@ -12,14 +12,14 @@ import javax.crypto.spec.IvParameterSpec;
 
 public class cyst01 {
   public static void main(String args[]) throws Exception {
-    ObjectInputStream ois = new ObjectInputStream(new FileInputStream("keyfile"));
+    ObjectInputStream ois = new ObjectInputStream(new FileInputStream("keyfile.txt"));
     DESKeySpec ks = new DESKeySpec((byte[]) ois.readObject());
     SecretKeyFactory skf = SecretKeyFactory.getInstance("DES");
     SecretKey key = skf.generateSecret(ks);
 
     Cipher c = Cipher.getInstance("DES/CFB8/NoPadding");
     c.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec((byte[]) ois.readObject()));
-    CipherInputStream cis = new CipherInputStream(new FileInputStream("ciphertext"), c);
+    CipherInputStream cis = new CipherInputStream(new FileInputStream("ciphertext.txt"), c);
     BufferedReader br = new BufferedReader(new InputStreamReader(cis));
     System.out.println(br.readLine());
   }
--- CYST/cyst02.java
+++ CYST/cyst02.java
@@ -19,12 +19,12 @@ public class cyst02 {
     SecretKeyFactory skf = SecretKeyFactory.getInstance("DES");
     Class spec = Class.forName("javax.crypto.spec.DESKeySpec");
     DESKeySpec ks = (DESKeySpec) skf.getKeySpec(key, spec);
-    ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("keyfile"));
+    ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("keyfile.txt"));
     oos.writeObject(ks.getKey());
 
     Cipher c = Cipher.getInstance("DES/CFB8/NoPadding");
     c.init(Cipher.ENCRYPT_MODE, key);
-    CipherOutputStream cos = new CipherOutputStream(new FileOutputStream("ciphertext"), c);
+    CipherOutputStream cos = new CipherOutputStream(new FileOutputStream("ciphertext.txt"), c);
     PrintWriter pw = new PrintWriter(new OutputStreamWriter(cos));
     pw.println("Stand and unfold yourself");
     pw.close();
